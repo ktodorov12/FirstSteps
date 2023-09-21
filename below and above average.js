4ac1c6c8d25f8f74dea8c4a3348d132339a3c99b
ktodorov12 <k_todorov12@abv.bg>
2023-09-21 23:09:23 +0300

    task

---
+++ below and above average.js
@@ -0,0 +1,26 @@
+let input = ["3,-12,0,0,13,5,1,0,-2"];
+
+let print = this.print || console.log;
+let gets =this.gets ||((arr, index) => () =>arr[index++])(input, 0);
+
+let numbers = gets().split(",");
+let above = [];
+let below = [];
+let sum = 0;
+
+for (let char of numbers){
+    char = Number(char)
+    sum += char
+}
+let average = sum / numbers.length;
+
+for (let i = 0; i < numbers.length; i++){
+    if (numbers[i] > average){
+        above.push(numbers[i]);
+    } else if(numbers[i] < average){
+        below.push(numbers[i])
+    }
+}
+console.log(`avg: ${average.toFixed(2)}`);
+console.log(`below: ${below}`);
+console.log(`above: ${above}`);
