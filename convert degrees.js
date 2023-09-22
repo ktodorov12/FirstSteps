4e75950169a140440d2fe442e1d685b46385007f
ktodorov12 <k_todorov12@abv.bg>
2023-09-22 17:38:21 +0300

    ubdate

--- convert degrees.js
+++ convert degrees.js
@@ -1,11 +1,7 @@
-let input = [0, 15, 30];
+let input = ['0 15 30'];
 
 let print = this.print || console.log;
 let gets =this.gets ||((arr, index) => () =>arr[index++])(input, 0);
 
 let array = gets().split(" ");
-
-for (let char of array){
-    char = char * 9/5 + 32;
-    console.log(Math.round(char));
-}
+array.forEach(element => console.log(element * 9/5 + 32));
