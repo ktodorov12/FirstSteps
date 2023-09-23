3b1571cb923113c7a82fde1d118a8e5c74f26104
ktodorov12 <k_todorov12@abv.bg>
2023-09-23 15:55:55 +0300

    task

--- !crooked digits.js
+++ !crooked digits.js
@@ -1,25 +1,21 @@
-let input = [1012919201902741904102947190247179024901n];
+let input = ['-7231'];
 
 let print = this.print || console.log;
 let gets = this.gets || ((arr, index) => () =>arr[index++])(input, 0);
 
-let digit = parseFloat(gets());
-digit = Math.abs(digit);
-digit = digit.toString().split(".").join("");
-let sum = 0;
-let newSum = 0;
+let digit = gets();
 
+do{
+  let sum = 0;
 
 for (let i = 0; i < digit.length; i++) {
-  sum += parseFloat(digit[i]);
-}
-while (sum > 9) {
-  sum = sum.toString();
-  newSum = 0;
-  for (let j = 0; j < sum.length; j++) {
-    newSum += parseFloat(sum[j]);
+  let num = +digit[i];
+  if (isNaN(num)){
+    continue;
   }
-  sum = newSum;
+  sum += num
 }
+digit = sum.toString();
+} while (digit.length > 1);
 
-console.log(sum);
+console.log(digit);
---
+++ !prime triangle.js
@@ -0,0 +1,5 @@
+let input = ['-7231'];
+
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () =>arr[index++])(input, 0);
+
