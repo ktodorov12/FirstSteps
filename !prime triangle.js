1f62cb75b8601c946a3fb631874df8073d96af90
ktodorov12 <k_todorov12@abv.bg>
2023-09-23 18:26:42 +0300

    task

--- !prime triangle.js
+++ !prime triangle.js
@@ -1,5 +1,29 @@
-let input = ['-7231'];
+let input = [10];
 
 let print = this.print || console.log;
-let gets = this.gets || ((arr, index) => () =>arr[index++])(input, 0);
+let gets =
+  this.gets ||
+  (
+    (arr, index) => () =>
+      arr[index++]
+  )(input, 0);
 
+let number = +gets();
+let line = "";
+
+for (let i = 1; i <= number; i++) {
+  let sqrt = Math.sqrt(i);
+  let isPrime = true;
+
+  for (let j = 2; j <= sqrt; j++) {
+    if (i % j === 0) {
+      isPrime = 0;
+    }
+  }
+  let printDigit = isPrime ? "1" : "0";
+  line += printDigit;
+
+  if (isPrime) {
+    console.log(line);
+  }
+}
