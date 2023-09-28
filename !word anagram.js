90d908ad7351d438062fe0971806a41ffca2e2c5
ktodorov12 <k_todorov12@abv.bg>
2023-09-28 19:27:46 +0300

    task

--- !word anagram.js
+++ !word anagram.js
@@ -1,23 +1,29 @@
 let input = ["anagram",9,"anagram","gramana","aaagrnm","margana",
-"aanagram","aaagram", "anagra","anagrama", "yxy"];
+"aanagram","aaagram", "anagra","anagrama", "yxy"];;
 
 let print = this.print || console.log;
 let gets =this.gets ||((arr, index) => () =>arr[index++])(input, 0);
 
 let word = gets();
 let tries = +gets();
-let madeWord = "";
 
 for (let i = 0; i < tries; i++) {
-  let attempt = gets();
-  if (attempt.length < word.length || attempt.length > word.length) {
-    console.log("No");
-    continue;
+  let attempt = gets()
+  let isAnagram = true;
+  if (attempt.length !== word.length) {
+    isAnagram = false;
+  } else {
+  for (let j = 0; j < word.length; j++){
+    if (word[j] !== attempt[j]){
+      isAnagram = false;
+      break;
+    }
   }
-  attempt.search(word);
-  if (word) {
+  if (isAnagram) {
     console.log("Yes");
   } else {
     console.log("No");
   }
 }
+}
+
