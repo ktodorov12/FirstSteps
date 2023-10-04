b8684c17819ab52a20e8dc10743d8e67961d6220
ktodorov12 <k_todorov12@abv.bg>
2023-10-04 20:39:20 +0300

    task

--- titleSearch.js
+++ titleSearch.js
@@ -1,41 +1,34 @@
-let input = ['peshoishere', 3, 'eho','piere','telerik'];
+let input = ['azsumtup',
+ 3, 'av','vvcxv','moccucomgic'];
 
 let print = this.print || console.log;
 let gets =this.gets ||((arr, index) => () =>arr[index++])(input, 0);
 
 let title = gets();
+let copy = title;
 let lines = +gets();
-let count = 0
-title = Array.from(title)
 
 for (let i = 0; i < lines; i++){
     let word = gets().toLowerCase();
-    let removed = ''
-
+    let indexCurr = 0
+    let isFound = true;
     for (let j = 0; j < word.length; j++){
-        let search = title.includes(word[i])
-
-        if (search){
-            count++
+        let curLetter = word[j]
+        let indexOfLetter = title.indexOf(curLetter, indexCurr)
+        if (indexOfLetter > -1){
+            title = title.substring(0, indexOfLetter) + title.substring(indexOfLetter + 1)
+            indexCurr = indexOfLetter
+        } else {
+            isFound = false;
+            break;
         }
     }
 
-    if (count === word.length){
-        
-        
-        for (let j = 0; j < word.length; j++){
-            for (let x = 0; x < title.length; x++){
-                if (word[j] === title[x]){
-                    title.splice(x, 1)
-                    break;
-                }
-            }
-        }
+    if (isFound){
+        copy = title;
+        console.log(title);
     } else {
         console.log("No such title found!");
-        continue;
+        title = copy
     }
-
-    count = 0;
-    console.log(title.join(''));
 }
