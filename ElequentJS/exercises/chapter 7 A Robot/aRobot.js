1546b354eb2ad2336afadbf96809744c4717516d
ktodorov12 <k_todorov12@abv.bg>
2024-04-26 17:21:00 +0300

    Create class thath tracks the position of the robots and the parcels. When the robot moves, data is updated in the move method

--- ElequentJS/exercises/chapter 7 A Robot/aRobot.js
+++ ElequentJS/exercises/chapter 7 A Robot/aRobot.js
@@ -15,6 +15,31 @@ const roads = [
   "Shop-Town Hall",
 ];
 
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    }
+
+    let parcels = this.parcels
+      .map((p) => {
+        if (p.place != this.place) {
+          return p;
+        }
+
+        return { place: destination, address: p.address };
+      })
+      .filter((p) => p.place != p.address);
+
+    return new VillageState(destination, parcels);
+  }
+}
+
 function buildGraph(edges) {
   let graph = Object.create(null);
 
@@ -37,4 +62,3 @@ function buildGraph(edges) {
 
 const roadGraph = buildGraph(roads);
 
-console.log(roadGraph);
