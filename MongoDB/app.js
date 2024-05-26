0cfd9ce6892ab76a37cb131620ed70525c85ade9
ktodorov12 <k_todorov12@abv.bg>
2024-05-26 20:38:58 +0300

    Setting up db

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const { connectToDb, getDb } = require("./db");
 
 //init app & middleware
 const app = express();
 const port = 5000;
 
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
+//db conncetion
+let db;
+
+connectToDb((err) => {
+  if (err) {
+    throw new Error(err);
+  }
+
+  app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+  });
+  db = getDb();
 });
 
 //routes
