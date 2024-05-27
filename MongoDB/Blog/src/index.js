434a90af5ea08b4044662fca75c8054b90618ec8
ktodorov12 <k_todorov12@abv.bg>
2024-05-27 21:32:17 +0300

    Create database configuration with mongoose

--- MongoDB/Blog/src/index.js
+++ MongoDB/Blog/src/index.js
@@ -4,10 +4,12 @@ const express = require("express");
 const mainRouter = require("./config/routes/main");
 const { ejsConfig } = require("./config/ejs");
 const { expressConfig } = require("./config/express");
+const connectDB = require("./config/db");
 
 const app = express();
 const port = 5000 || process.env.PORT;
 
+connectDB();
 ejsConfig(app);
 expressConfig(app);
 app.use(mainRouter);
