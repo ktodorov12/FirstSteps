c2a7c4d28023b11e1632f1ae98d9222c9f6c385f
ktodorov12 <k_todorov12@abv.bg>
2024-06-03 23:51:36 +0300

    attach the admin router

--- MongoDB/Blog/src/index.js
+++ MongoDB/Blog/src/index.js
@@ -5,6 +5,7 @@ const mainRouter = require("./config/routes/main");
 const { ejsConfig } = require("./config/ejs");
 const { expressConfig } = require("./config/express");
 const connectDB = require("./config/db");
+const adminRouter = require("./config/routes/admin");
 
 const app = express();
 const port = 5000 || process.env.PORT;
@@ -13,6 +14,7 @@ connectDB();
 ejsConfig(app);
 expressConfig(app);
 app.use(mainRouter);
+app.use(adminRouter)
 
 app.listen(port, () => {
   console.log(`App is listening on port: ${port}`);
