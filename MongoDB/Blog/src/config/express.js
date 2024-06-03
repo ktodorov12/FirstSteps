d43b260cf3be27ca580c86f1b34033707105c6c2
ktodorov12 <k_todorov12@abv.bg>
2024-06-03 23:51:27 +0300

    Udate express configurations

--- MongoDB/Blog/src/config/express.js
+++ MongoDB/Blog/src/config/express.js
@@ -1,8 +1,24 @@
 const { urlencoded, static: staticExpress } = require("express");
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
+const session = require("express-session");
+const metodOverride = require("method-override");
 
 module.exports = {
   expressConfig: (app) => {
     app.use(urlencoded({ extended: true }));
     app.use("/static", staticExpress("static"));
+    app.use(cookieParser);
+    app.use(
+      session({
+        secret: "keyborad cat",
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({
+          mongoUrl: process.env.MONGODB_URI,
+        }),
+      })
+    );
+    app.use(metodOverride("_method"));
   },
 };
