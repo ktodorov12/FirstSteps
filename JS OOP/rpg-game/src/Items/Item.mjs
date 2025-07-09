8106cc5a7c9cd4c12484f7452be1d693586bfb9d
ktodorov12 <k_todorov12@abv.bg>
2025-07-09 18:35:14 +0300

    create item class with base functionality

---
+++ JS OOP/rpg-game/src/Items/Item.mjs
@@ -0,0 +1,10 @@
+export default class Item {
+  constructor(type, name, stat, statType, bodyPart, rarity) {
+    this.type = type;
+    this.name = name;
+    this.stat = stat;
+    this.statType = statType;
+    this.bodyPart = bodyPart;
+    this.rarity = rarity;
+  }
+}
