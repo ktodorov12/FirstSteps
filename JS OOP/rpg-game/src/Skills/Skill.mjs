907e322e62280570708381d0d12dbe1765399d2a
ktodorov12 <k_todorov12@abv.bg>
2025-07-09 18:35:21 +0300

    create skill class with base functionality

---
+++ JS OOP/rpg-game/src/Skills/Skill.mjs
@@ -0,0 +1,8 @@
+export default class Skill {
+  constructor(name, manaCost, stat, statType) {
+    this.name = name;
+    this.manaCost = manaCost;
+    this.stat = stat;
+    this.statType = statType;
+  }
+}
